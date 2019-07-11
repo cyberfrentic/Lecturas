a0b4969cec48022c5467cbe92a786d5d4635caa9
cyberfrentic <j_hugo_p_c3@msn.com>
2019-07-11 14:47:17 -0600

    Modificando el menú de slider draw

--- app/src/main/java/com/example/lecturas/navigationDrawActiviy.java
+++ app/src/main/java/com/example/lecturas/navigationDrawActiviy.java
@@ -1,7 +1,8 @@
 package com.example.lecturas;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +15,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
 public class navigationDrawActiviy extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,14 +27,46 @@ public class navigationDrawActiviy extends AppCompatActivity
         setContentView(R.layout.activity_navigation_draw_activiy);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+    // ################ eventos action buton del menu button
+        //#################evento 1 lectur nuevas
+        final FloatingActionsMenu menuBotones = (FloatingActionsMenu) findViewById(R.id.grupoFab);
+        final FloatingActionButton fab1 = findViewById(R.id.fab1);
+        final FloatingActionButton fab2 = findViewById(R.id.fab2);
+        final FloatingActionButton fab3 = findViewById(R.id.fab3);
+
+        fab1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Agregar lectura", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Lectura Nueva", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                menuBotones.collapse();
             }
         });
+
+        //############# evento 2 lista de lecturas
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Lista de lecturas", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                menuBotones.collapse();
+            }
+        });
+
+        //############# evento 3 editar lectura
+
+        fab3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Lista de lecturas", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                menuBotones.collapse();
+            }
+        });
+    //############################################################################
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -89,7 +125,9 @@ public class navigationDrawActiviy extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-
+            Intent salida = new Intent( Intent.ACTION_MAIN); //Llamando a la activity principal
+            finish(); // La cerramos.
+            System.exit(0);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
